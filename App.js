774fa38198babf0c562937b4a004580a43fc133d
lmvit <85612558+lmvit@users.noreply.github.com>
2022-05-10 12:45:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import {useEffect} from 'react'; 
+import './App.css';
+import CourseCard from './components/CourseCard'
+
+function App() {
+
+  const loadScript = (src) => {
+    return new Promise((resolve) => {
+      const script = document.createElement('script')
+
+      script.src = src
+      script.onload = () => {
+        resolve(true)
+      }
+      script.onerror = () => {
+        resolve(false)
+      }
+      document.body.appendChild(script)
+    })
+  }
+  useEffect(() => {
+    loadScript("https://checkout.razorpay.com/v1/checkout.js")
+  })
+  return (
+    <section>
+      <CourseCard/>
+    </section>
+  );
+
+}
+
+export default App;
